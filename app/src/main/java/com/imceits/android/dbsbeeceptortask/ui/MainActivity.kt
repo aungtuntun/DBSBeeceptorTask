ff4c44b31cbb3afc7147437ec1ffafd1cf71166c
aungtuntun <aungtuntun2012@gmail.com>
2020-04-19 05:33:43 +0800

    Modified navigation layout

--- app/src/main/java/com/imceits/android/dbsbeeceptortask/ui/MainActivity.kt
+++ app/src/main/java/com/imceits/android/dbsbeeceptortask/ui/MainActivity.kt
@@ -1,12 +1,10 @@
 package com.imceits.android.dbsbeeceptortask.ui
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
 import com.imceits.android.dbsbeeceptortask.R
 
 class MainActivity : AppCompatActivity() {
@@ -14,17 +12,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
         val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(setOf(
-            R.id.navigation_home,
-            R.id.navigation_dashboard,
-            R.id.navigation_notifications
+            R.id.nav_article
         ))
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+     //   setupActionBarWithNavController(navController, appBarConfiguration)
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
