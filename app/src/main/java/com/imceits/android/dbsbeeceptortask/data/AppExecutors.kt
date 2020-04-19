6b7bf530f2a6011b58a5274ad90747d1a7243f12
aungtuntun <aungtuntun2012@gmail.com>
2020-04-20 02:55:47 +0800

    Creating multiple Executors for separated threads

---
+++ app/src/main/java/com/imceits/android/dbsbeeceptortask/data/AppExecutors.kt
@@ -0,0 +1,36 @@
+package com.imceits.android.dbsbeeceptortask.data
+
+import android.os.Handler
+import android.os.Looper
+import java.util.concurrent.Executor
+import java.util.concurrent.Executors
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+open class AppExecutors (private val diskIO: Executor, private val network: Executor,
+ private val mainThread: Executor) {
+
+    @Inject
+    constructor(): this(Executors.newSingleThreadExecutor(),
+    Executors.newFixedThreadPool(4),
+    MainThreadExecutor())
+
+    fun diskIO(): Executor {
+        return diskIO
+    }
+
+    fun networkIO(): Executor {
+        return networkIO()
+    }
+    fun mainThread(): Executor {
+        return mainThread
+    }
+    public class MainThreadExecutor : Executor{
+        private val mainThreadHandler = Handler(Looper.getMainLooper())
+        override fun execute(command: Runnable) {
+            mainThreadHandler.post(command)
+        }
+
+    }
+}
