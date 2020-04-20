de07caa59b8d70d4dd7879f803398941fe70ea8e
aungtuntun <aungtuntun2012@gmail.com>
2020-04-20 14:04:17 +0800

    Creating Repository for implementation of data source management and functions update, get

---
+++ app/src/main/java/com/imceits/android/dbsbeeceptortask/data/ArcRepository.kt
@@ -0,0 +1,45 @@
+package com.imceits.android.dbsbeeceptortask.data
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import retrofit2.Call
+import javax.inject.Singleton
+
+@Singleton
+class ArcRepository constructor(private val apiService: APIService, private val arcDao: ArcDao,
+private val appExecutors: AppExecutors) {
+
+    fun loadArticles(): LiveData<Resource<List<Article>>> {
+        return object : NetworkBoundResource<List<Article>, List<Article>> (appExecutors) {
+            override fun saveCallResult(item: List<Article>) {
+                arcDao.insert(item)
+            }
+
+            override fun shouldFetch(data: List<Article>?): Boolean {
+                return data.isNullOrEmpty()
+            }
+
+            override fun loadFromDb(): LiveData<List<Article>> {
+                return arcDao.getArticle()
+            }
+
+            override fun createCall(): Call<List<Article>> {
+                return apiService.getArticles()
+            }
+
+        }.asLiveData()
+    }
+
+    fun saveArticle(item: Article): LiveData<Long> {
+        val result = MutableLiveData<Long>()
+        appExecutors.diskIO().execute {
+            val id = arcDao.save(item)
+            result.postValue(id)
+        }
+        return result
+    }
+
+    fun getArticle(id: Int): LiveData<Article> {
+        return arcDao.get(id)
+    }
+}
