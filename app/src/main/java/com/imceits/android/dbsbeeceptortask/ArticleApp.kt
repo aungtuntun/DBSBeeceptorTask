9d7cbe6627989cedad79f561dfc15abd78610121
aungtuntun <aungtuntun2012@gmail.com>
2020-04-20 16:39:11 +0800

    Injecting App to AppInjector

--- app/src/main/java/com/imceits/android/dbsbeeceptortask/ArticleApp.kt
+++ app/src/main/java/com/imceits/android/dbsbeeceptortask/ArticleApp.kt
@@ -1,10 +1,22 @@
 package com.imceits.android.dbsbeeceptortask
 
 import android.app.Application
+import com.imceits.android.dbsbeeceptortask.di.AppInjector
+import dagger.android.AndroidInjector
+import dagger.android.DispatchingAndroidInjector
+import dagger.android.HasAndroidInjector
+import javax.inject.Inject
 
-class ArticleApp: Application() {
+class ArticleApp: Application(), HasAndroidInjector {
+    @Inject
+    lateinit var dispatchingAndroidInjector: DispatchingAndroidInjector<Any>
 
     override fun onCreate() {
         super.onCreate()
+        AppInjector.init(this)
+    }
+
+    override fun androidInjector(): AndroidInjector<Any> {
+        return dispatchingAndroidInjector
     }
 }
