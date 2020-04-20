671b2b5affdc2486f2b4b4becf9cb5e8ff82bd35
aungtuntun <aungtuntun2012@gmail.com>
2020-04-20 22:15:02 +0800

    Fetching ViewModel into data binding

--- app/src/main/java/com/imceits/android/dbsbeeceptortask/ui/ArticleFragment.kt
+++ app/src/main/java/com/imceits/android/dbsbeeceptortask/ui/ArticleFragment.kt
@@ -4,14 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.DefaultItemAnimator
-import com.imceits.android.dbsbeeceptortask.R
-import com.imceits.android.dbsbeeceptortask.data.AbsentLiveData
 import com.imceits.android.dbsbeeceptortask.data.AutoClearedValue
 import com.imceits.android.dbsbeeceptortask.databinding.FragmentArticleBinding
 import com.imceits.android.dbsbeeceptortask.di.Injectable
